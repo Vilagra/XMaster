580eae4304ec8cf50ce1dba2bb43ff0879c8da3d
Vilagra <Vilagra@mail.ru>
2019-07-10 13:56:14 +0300

    change call.execute() to suspend function

--- app/src/main/java/com/example/xmaster/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/xmaster/data/RepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.xmaster.data
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.paging.LivePagedListBuilder
@@ -11,11 +12,21 @@ import com.example.xmaster.data.model.Coin
 import com.example.xmaster.data.network.ConnectivityDispatcher
 import com.example.xmaster.data.network.RetrofitHelper
 import com.example.xmaster.utils.Constants
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 
-class RepositoryImpl constructor(val connectivityDispatcher: ConnectivityDispatcher, val appDataBase: AppDataBase, val context: Context) : Repository {
+class RepositoryImpl constructor(
+    val connectivityDispatcher: ConnectivityDispatcher,
+    val appDataBase: AppDataBase,
+    val context: Context
+) : Repository {
 
 
     override fun getAllCoinsFromDb(): MediatorLiveData<ResultWrapper<PagedList<Coin>>> {
@@ -27,17 +38,20 @@ class RepositoryImpl constructor(val connectivityDispatcher: ConnectivityDispatc
         return result
     }
 
-    fun loadPictures() {
+    suspend fun loadPictures() {
         val lictCoins = appDataBase.coinsDao().getAllCoinsList().toSet()
         val coinsWithoutPicture = lictCoins.filter { it.imageURL == null }
         for (lictCoin in coinsWithoutPicture.chunked(500)) {
             val coinsWithoutPictureIDsString = lictCoin.map { it.id }.joinToString(separator = ",")
-            val result = RetrofitHelper.authService.getPicture(coinsWithoutPictureIDsString).execute()
-            if(result.isSuccessful){
-                result.body()?.res?.forEach { images -> coinsWithoutPicture.find { coin -> coin.id.toInt() === images.id.toInt() }?.imageURL = images.logo;
+            try {
+                val result = RetrofitHelper.authService.getPicture(coinsWithoutPictureIDsString).suspendExecute()
+                result.res?.forEach { images ->
+                    coinsWithoutPicture.find { coin -> coin.id.toInt() === images.id.toInt() }?.imageURL = images.logo;
                     Glide.with(context)
                         .load(images.logo)
-                        .preload(500, 500)}
+                        .preload(500, 500)
+                }
+            } catch (e: BadResponceException) {
             }
         }
         appDataBase.coinsDao().update(coinsWithoutPicture)
@@ -48,8 +62,9 @@ class RepositoryImpl constructor(val connectivityDispatcher: ConnectivityDispatc
         livedata.postValue(ResultWrapper.loading(Unit))
         if (!connectivityDispatcher.hasConnection()) {
             livedata.postValue(ResultWrapper.error(Constants.LOST_INTERNET_CONNECTION, Unit))
-        } else{
-            GlobalScope.launch {
+        } else {
+            MainScope().launch {
+                Log.d("thread", "Default               : I'm working in thread ${Thread.currentThread().name}")
                 loadCoinsFromNetwork(livedata)
             }
         }
@@ -57,20 +72,38 @@ class RepositoryImpl constructor(val connectivityDispatcher: ConnectivityDispatc
     }
 
     suspend fun loadCoinsFromNetwork(liveData: MutableLiveData<ResultWrapper<Unit>>) {
-        val result = RetrofitHelper.authService.getAll().execute()
-        if (result.isSuccessful) {
-            result.body()?.coins?.let {
-                appDataBase.coinsDao().insert(it)
-                liveData.postValue(ResultWrapper.success(Unit))
-                loadPictures()
-            } ?: liveData.postValue(
+        withContext(Dispatchers.IO) {
+            try {
+                val result = RetrofitHelper.authService.getAll().suspendExecute()
+                result.coins?.let {
+                    appDataBase.coinsDao().insert(it)
+                    liveData.postValue(ResultWrapper.success(Unit))
+                    loadPictures()
+                }
+            } catch (e: BadResponceException) {
                 ResultWrapper.error(
-                    result.errorBody()?.string() ?: "coins==null",
+                    e.errorMessage ?: "coins==null",
                     liveData.value?.data
                 )
-            )
-        } else {
-            liveData.postValue(ResultWrapper.error(Constants.SERVER_PROBLEM ?: "", Unit))
+            }
         }
     }
 }
+
+suspend fun <T> Call<T>.suspendExecute(): T {
+    return suspendCoroutine { continuation ->
+        val result = execute();
+        if (result.isSuccessful) {
+            result.body()?.let { continuation.resume(it) } ?: continuation.resumeWithException(
+                BadResponceException(
+                    result.errorBody()?.string()
+                )
+            )
+        } else {
+            continuation.resumeWithException(BadResponceException(null))
+        }
+    }
+}
+
+
+class BadResponceException(val errorMessage: String?) : Exception() {}
