d6838d9811b87d7122ca7d908a4792b8f48ec83b
Vilagra <levenko.aleksandr5@gmail.com>
2020-04-02 19:11:16 +0300

    google login integration

--- app/src/main/java/com/example/xmaster/ui/login/main/MainLoginFragment.kt
+++ app/src/main/java/com/example/xmaster/ui/login/main/MainLoginFragment.kt
@@ -9,39 +9,84 @@ import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
-import com.facebook.login.widget.LoginButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
+
+private const val RC_SIGN_IN = 1
 
 class MainLoginFragment : BaseFragment<LoginFragmentBinding>() {
 
     val callbackManager = CallbackManager.Factory.create()
+    lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override val contentLayoutId: Int
         get() = R.layout.login_fragment
 
     override fun setupBinding() {
-        val loginButton = binding.facebookButton
-        loginButton.setPermissions("email")
-        loginButton.setFragment(this)
+        configureFacebookLoginButton()
+        configureGoogleLogin()
+    }
 
-        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
-            override fun onSuccess(loginResult: LoginResult?) { // App code
-                Log.d("facebook_login", loginResult?.accessToken?.token ?: "")
+    private fun configureFacebookLoginButton(){
+        binding.facebookButton.run {
+            setPermissions("email")
+            setFragment(this@MainLoginFragment)
+            registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {}
+                override fun onCancel() {}
+                override fun onError(exception: FacebookException) {}
+            })
+        }
+    }
 
+    private fun configureGoogleLogin(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+        binding.googleButton.run {
+            setOnClickListener { view ->
+                signIn()
             }
+        }
+    }
 
-            override fun onCancel() { // App code
-                Log.d("facebook_login", "cancel")
-            }
-
-            override fun onError(exception: FacebookException) {
-                Log.d("facebook_login", exception.toString())// App code
-            }
-        })
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
+        //TODO if account isn't null that means user is already logged in, write
+        // logic which directs user to next screen
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        callbackManager.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            RC_SIGN_IN -> {
+                val task =
+                    GoogleSignIn.getSignedInAccountFromIntent(data)
+                handleSignWithGoogleInResult(task)
+            }
+            else -> callbackManager.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun handleSignWithGoogleInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+            Log.d("handle_sign_in", account?.email ?: "null")
+        } catch (e: ApiException) {
+            Log.d("handle_sign_in", "signInResult:failed code=" + e.statusCode)
+        }
     }
 }
